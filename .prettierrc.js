bbfc9b434b49d71a61926a5c45a455c0e337b7e6
raaynaldo <raaynaldo@hotmail.com>
2021-03-13 11:35:49 -0500

    tailwind setup

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  overrides: [
+    {
+      files: '*.mdx',
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
+};
---
+++ pages/auth/login.js
@@ -0,0 +1,3 @@
+export default function login() {
+  return <div className='flex justify-center'>login</div>;
+}
