29c6c827431431d1228ddfa1a63e3a94d078078b
raaynaldo <raaynaldo@hotmail.com>
2021-03-13 19:07:29 -0500

    login via google.

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,3 +1,21 @@
+import { useAuth } from '@/lib/auth';
+
 export default function login() {
-  return <div className='flex justify-center'>login</div>;
+  const auth = useAuth();
+
+  return (
+    <div className='flex justify-center'>
+      <div className='text-center'>
+        <h2>login</h2>
+        <h3>
+          Current User: <span>{auth.user?.email}</span>
+        </h3>
+        {auth.user ? (
+          <button onClick={(e) => auth.signout()}>Sign out</button>
+        ) : (
+          <button onClick={(e) => auth.siginWithGoogle()}>Sign In</button>
+        )}
+      </div>
+    </div>
+  );
 }
