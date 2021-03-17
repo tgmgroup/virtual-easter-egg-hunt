59411bbb9a927ed42e7985496fd7708fc509d7c0
raaynaldo <raaynaldo@hotmail.com>
2021-03-17 17:36:24 -0400

    conditional login header.

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,31 @@
 import Link from 'next/link';
+import { useAuth } from '@/lib/auth';
 
 export default function Header() {
+  const { user } = useAuth();
+
   return (
     <header className='shadow-xl'>
       <div className='flex items-center justify-between h-16 layout'>
         <Link href='/'>
-          <a href='#' className='text-3xl text-primary'>
+          <a href='#' className='text-xl md:text-3xl text-primary'>
             Virtual Egg Hunt
           </a>
         </Link>
         <nav className='space-x-5'>
-          <Link href='/auth/login'>
-            <a href='#' className='text-lg text-gray-800'>
-              Log in
-            </a>
-          </Link>
+          {user ? (
+            <div className='flex items-center space-x-3'>
+              <p className='hidden text-xl md:block'>Hello, {user.name}!</p>
+              <button className='text-sm md:text-md btn'>Profile</button>
+            </div>
+          ) : (
+            <Link href='/auth/login'>
+              <a href='#' className='text-lg text-gray-800'>
+                Log in
+              </a>
+            </Link>
+          )}
+
           {/* <a href='#' className='text-lg text-gray-800'>
               Sign Up
             </a> */}
